b646d420a832657bddb7f419c782dfaa5dae06a3
lunae3887 <40661841+lunae3887@users.noreply.github.com>
2018-06-28 18:11:05 +0700

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,15 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, $http) {
+	function getPosts() {
+		return $http.get('https://jsonplaceholder.typicode.com/posts').then(function (result) {
+			console.log(result.data)
+			$scope.posts = result.data
+		})
+	}
+
+	getPosts()
+
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
